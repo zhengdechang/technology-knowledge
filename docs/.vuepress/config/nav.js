3411fec227e731442676266df20879fe32877d56
dechang zheng <2473023641@qq.com>
2023-10-09 10:02:52 +0000

    add ansible document

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -45,7 +45,7 @@ module.exports = [
     link: "/technology/",
     items: [
       { text: "Linux", link: "/pages/64df52/" },
-      // { text: "GitHub技巧", link: "/pages/4c778760be26d8b3/" },
+      { text: "Ansible", link: "/pages/b24547/" },
       // { text: "Nodejs", link: "/pages/117708e0af7f0bd9/" },
       // { text: "博客搭建", link: "/pages/41f87d890d0a02af/" },
     ],
